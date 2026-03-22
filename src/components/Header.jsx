d0f90c59335579868124191e37d1630e80733727
Sereja_Lebeda <lserg97@gmail.com>
2026-03-22 12:21:14 +0300

    Feat: header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
-    <div className="flex justify-between items-center w-full text-text-primary select-none">
-      <div className="flex justify-start items-center ">logo</div>
+    <div className="flex justify-center items-center w-full text-text-primary gap-12">
+      <div className="flex justify-start items-center ">
+        <img
+          src="/pics/AppLogoCropped.png"
+          alt="logo"
+          className="w-12 h-12 object-contain object-center rounded"
+        />
+      </div>
 
       <div className="flex justify-center items-center text-xl">
         {/* TODO: Add dynamic time and username */}
